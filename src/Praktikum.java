d219358f5015ab1c886454bcc82e92acaf68ecc1
Bagrat Work <bagratavanesian@yandex.ru>
2022-06-02 16:45:19 +0300

    Доработал условия проверки високосности

---
+++ src/Praktikum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+        //Вызов метода определения високосности года. True - 12.09, False - 13.09
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+    // Метод определяет високосность года по условиям
+    public static boolean isLeapYear(int year) {
+        //Если год (кратный 4 но некратный  100) или (кратный 400) - то он високосный, иначе - нет
+        return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+    }
+}
